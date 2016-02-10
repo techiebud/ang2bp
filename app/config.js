4bedbbc337153f5013d01fa85fbea1d0fe404275
Johnny Sims <techiebud@gmail.com>
2016-02-09 20:48:53 -0500

    init

---
+++ app/config.js
@@ -0,0 +1,34 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  typescriptOptions: {
+    "module": "system",
+    "experimentalDecorators": true
+  },
+  paths: {
+    "npm:*": "jspm_packages/npm/*",
+    "github:*": "jspm_packages/github/*",
+    "app": "src"
+  },
+
+  packages: {
+    "app": {
+      "main": "app",
+      "defaultExtension": "ts",
+      "meta": {
+        "*.ts": {
+          "loader": "typescript"
+        }
+      },
+      "map": {
+        "typescript": "npm:typescript@1.8.0"
+      }
+    }
+  },
+
+  map: {
+    
+    "typescript": "npm:typescript@1.8.0"
+  }
+  });
---
+++ app/src/app.ts
@@ -0,0 +1,29 @@
+/*//if using babel or typescript compiler:
+import {
+  Component,
+  View,
+  bootstrap
+} from 'angular2\angular2;
+
+
+
+//create a simple angular component
+@Component({
+  selector: 'test-app'
+})
+@View({
+  template: '<h4>Hello {{name}}</h4>'
+})
+class TestApp {
+  name: string;
+  constructor(){
+    this.name = 'Angular2';
+    setTimeout(() => {
+      this.name = 'Angular2!!!'
+    },1500);
+  }
+}
+
+//start our app
+bootstrap(TestApp);*/
+console.log("Hello Techiebud");
